0065d428841caddc867e9be2277a34f58de6a5cb
shayan <sak704@nyu.edu>
2022-03-06 19:34:30 -0800

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let subBut = document.getElementById("submit-button");
+  // console.log(subBut)
+
+
+  subBut.addEventListener("click", addTasks);
+
+
+
 });
+
+function addTasks(e) {
+  e.preventDefault();
+  let list = document.getElementById("tasks");
+  let liTask = document.getElementById("new-task-description").value;
+  let node = document.createElement("li");
+  let delBut = document.createElement("button");
+  let prio = document.getElementById("priority-dropdown").value;
+
+  let hipri = [];
+  let medpri = [];
+  let lopri = [];
+
+
+  // console.log(prio);
+
+  if (prio === "high-priority") {
+
+    node.classList.add("high-priority");
+    
+  } else if (prio === "medium-priority") {
+
+    node.classList.add("medium-priority");
+    
+  } else if (prio === "low-priority") {
+
+    node.classList.add("medium-priority");
+
+  }
+
+  // console.log(node);
+
+  
+
+
+  delBut.append("X");
+  
+  node.append(`${liTask} `);
+
+  node.append(delBut);
+  
+  list.append(node);   
+  
+  
+}
